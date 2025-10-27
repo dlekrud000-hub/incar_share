8d21b9060ec02713e8d46525fe8defc80fad1be9
dlekrud000-hub <dlekrud000@gmail.com>
2025-10-27 13:43:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -203,7 +203,7 @@ document.addEventListener('DOMContentLoaded', function() {
             margin: 0,
             image: { type: 'jpeg', quality: 0.98 },
             html2canvas: { 
-                scale: 1.4, // ⭐️ scale 값을 1.4로 낮춰 A4 용지에 확실하게 맞도록 조정 (핵심)
+                scale: 1.3, // ⭐️ scale 값을 1.3로 낮춰 A4 용지에 확실하게 맞도록 조정 (핵심)
                 letterRendering: true // 텍스트 렌더링 품질 개선
             },
             jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
@@ -473,3 +473,4 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
